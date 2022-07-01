a915e615371290b55505cb61a6d6c9d7fd1843ef
ooshimo <103427458+ooshimo@users.noreply.github.com>
2022-07-01 18:46:00 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -9,3 +9,65 @@ if(e&&1===a.nodeType)while(c=e[d++])a.removeAttribute(c)}}),hb={set:function(a,b
 	})
 
 })(jQuery);
+
+$(function() {
+	//画面をスクロールするとイベントが発動する
+	$(window).scroll(function() {
+	  
+	  //フェードインさせたい要素の位置をずらす
+	  $('.fadein').css({
+		'opacity': '0',
+		'transform': 'translateX(800px)'
+	  });
+	  
+	  //スクロールバーの位置を取得
+	  var scroll = $(window).scrollTop();
+  
+	  //ウィンドウの高さを取得
+	  var windowHeight = $(window).height();
+  
+	  $('.fadein').each(function() {
+		//フェードインさせたい要素の縦位置を取得
+		var elemPos = $(this).offset().top;
+  
+		//要素がウィンドウの中に入ってからさらに100pxスクロールしたら要素をフェードインする
+		if (scroll > elemPos - windowHeight + 100) {
+		  $(this).css({
+			'opacity': '1',
+			'transform': 'translateX(0)'
+		  });
+		}
+	  });
+	});
+  });
+
+  $(function() {
+	//画面をスクロールするとイベントが発動する
+	$(window).scroll(function() {
+	  
+	  //フェードインさせたい要素の位置をずらす
+	  $('.fadein1').css({
+		'opacity': '0',
+		'transform': 'translateX(-800px)'
+	  });
+	  
+	  //スクロールバーの位置を取得
+	  var scroll = $(window).scrollTop();
+  
+	  //ウィンドウの高さを取得
+	  var windowHeight = $(window).height();
+  
+	  $('.fadein1').each(function() {
+		//フェードインさせたい要素の縦位置を取得
+		var elemPos = $(this).offset().top;
+  
+		//要素がウィンドウの中に入ってからさらに100pxスクロールしたら要素をフェードインする
+		if (scroll > elemPos - windowHeight + 100) {
+		  $(this).css({
+			'opacity': '1',
+			'transform': 'translateX(0)'
+		  });
+		}
+	  });
+	});
+  });
